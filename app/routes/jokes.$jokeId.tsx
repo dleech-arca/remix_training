5518212ab5b3d6b31ec26bf9e6a8e9087c54aef5
dleech-arca <152179100+dleech-arca@users.noreply.github.com>
2023-12-12 16:16:02 +0000

    Optimistic UI - add Optimistic UI functionality to the new jokes and jokeId pages. As a test you can throttle the page for slow 3G connectivity and create a new joke which will send you to the new joke where the delete button will be greyed out for a few seconds.

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -2,6 +2,7 @@ import type { ActionFunctionArgs, LoaderFunctionArgs, MetaFunction, } from "@rem
 import { json, redirect } from "@remix-run/node";
 import { isRouteErrorResponse, Form, Link, useLoaderData, useParams, useRouteError,} from "@remix-run/react";
 
+import {JokeDisplay} from "~/components/joke";
 import { db } from "~/utils/db.server";
 import { getUserId, requireUserId } from "~/utils/session.server";
 
@@ -75,23 +76,7 @@ export default function JokeRoute() {
     const data = useLoaderData<typeof loader>();
 
     return (
-        <div>
-            <p>Here's your hilarious joke:</p>
-            <p>{data.joke.content}</p>
-            <Link to=".">"{data.joke.name}" Permalink</Link>
-            {data.isOwner ? (
-                <Form method="post">
-                    <button
-                        className="button"
-                        name="intent"
-                        type="submit"
-                        value="delete"
-                    >
-                        Delete
-                    </button>
-                </Form>
-                ) : null}
-        </div>
+        <JokeDisplay isOwner={data.isOwner} joke={data.joke} />
     );
 }
 
